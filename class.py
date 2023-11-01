8774fb10a15331bddcadb68ca3ecdcd94db980be
Mofasanya07 <149578978+Mofasanya07@users.noreply.github.com>
2023-11-01 12:12:53 +0100

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class function():
+    def display():
+        p = int(input(' P = '))
+        r = int(input(' R = '))
+        t = int(input(' T = '))
+        y =100
+        x = p * r * t / 100
+        print(x)
+
+first_display = function
+first_display.display()
